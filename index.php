d045ee1bb5a659c1fb80ae4931106b62dc2fd91b
SantipapP <santipap.poorab@gmail.com>
2021-10-20 23:10:11 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="mheader">
+        <h2 class="text-center">ระบบรับสมัคร์พนักงาน</h2>
+    </div>
+    <div class="content">
+        <button onClick = location="register.php" class="logbtn">กดเพื่อสมัคร์งาน</button><br><br>
+        <label for="">หรือ</label><br><br>
+        <button onClick = location="login.php" class="btn">ตรวจสอบผลการสมัคร์</button><br><br>
+    </div>
+
+</body>
+</html>
